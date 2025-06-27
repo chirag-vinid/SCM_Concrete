fc295de947a4ca3163f07746d968ec8e6ccb37f8
UnitedFan06 <63413586+UnitedFan06@users.noreply.github.com>
2025-06-27 16:39:48 +0530

    Add files via upload

---
+++ mpu6050_logger.py
@@ -0,0 +1,34 @@
+from flask import Flask, request
+import pandas as pd
+from datetime import datetime
+import os
+
+app = Flask(__name__)
+
+FILE_NAME = "D:/Personal/SCM_Internship/Data_privatisation/MPU6050/MPU6050_Log.csv"
+
+@app.route("/update", methods=['POST'])
+def update():
+    try:
+        data = request.json
+        new_entry = {
+            'Timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            'GyroX_dps': data.get('gyroX_dps'),
+            'GyroY_dps': data.get('gyroY_dps'),
+            'GyroZ_dps': data.get('gyroZ_dps'),
+            'Accel_Euclidean': data.get('euclidean_sum_accel')
+        }
+
+        if os.path.exists(FILE_NAME) and os.path.getsize(FILE_NAME) > 0:
+            df = pd.read_csv(FILE_NAME)
+            df = pd.concat([df, pd.DataFrame([new_entry])], ignore_index=True)
+        else:
+            df = pd.DataFrame([new_entry])
+
+        df.to_csv(FILE_NAME, index=False)
+        return {'status': 'success'}, 200
+    except Exception as e:
+        return {'error message': str(e)}, 500
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
