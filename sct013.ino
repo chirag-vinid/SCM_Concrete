7a0b05eaebebce945b5461f67917a5eaea3356ba
Chirag Vinid <63413586+chirag-vinid@users.noreply.github.com>
2025-07-01 14:38:54 +0530

    Update sct013.ino

--- sct013.ino
+++ sct013.ino
@@ -1,31 +1,108 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 #include "EmonLib.h"
 
-EnergyMonitor emon1;
+// Wi-Fi credentials
+const char* ssid = "Airtel_9962583910";
+const char* password = "air10719";
 
-const int ANALOG_PIN = A0;
-const float CALIBRATION_CONSTANT = 30.0;
-const int NUM_SAMPLES = 1480;
+// Flask server IP
+const char* flask_server_ip = "192.168.1.3";
+const int flask_port = 5000;
+
+// LED Pin and Blinking
+const int ledPin = D4;
+unsigned long previousMillisLED = 0;
+const long blinkInterval = 500;
+bool ledState = LOW;
+
+// SCT-013 Current Sensor
+EnergyMonitor emon1;
+const int SCT013_ANALOG_PIN = A0;
+const float SCT013_CALIBRATION_CONSTANT = 30.0; // Calibrate this value!
+const int SCT013_NUM_SAMPLES = 1480; // For 50Hz mains
+
+WiFiClient client;
 
 void setup() {
   Serial.begin(115200);
+  delay(100);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
 
-  emon1.current(ANALOG_PIN, CALIBRATION_CONSTANT);
+  // Connect to Wi-Fi
+  Serial.print("Connecting to WiFi: "); Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi Connected. IP: " + WiFi.localIP().toString());
+  digitalWrite(ledPin, LOW);
 
-  delay(1000);
+  // SCT-013 Setup
+  emon1.current(SCT013_ANALOG_PIN, SCT013_CALIBRATION_CONSTANT);
+  Serial.println("SCT-013 setup complete.");
 }
 
 void loop() {
-  double Irms = emon1.calcIrms(NUM_SAMPLES);
+  unsigned long currentMillis = millis();
 
-  Serial.print("Current: ");
+  // LED blinking
+  if (WiFi.status() == WL_CONNECTED) {
+    if (currentMillis - previousMillisLED >= blinkInterval) {
+      previousMillisLED = currentMillis;
+      ledState = !ledState;
+      digitalWrite(ledPin, ledState ? HIGH : LOW);
+    }
+  } else {
+    digitalWrite(ledPin, HIGH);
+  }
+
+  // Reconnect Wi-Fi
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("WiFi Disconnected. Reconnecting...");
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    Serial.println("\nWiFi Reconnected. IP: " + WiFi.localIP().toString());
+    digitalWrite(ledPin, LOW);
+    previousMillisLED = currentMillis;
+  }
+
+  // Read SCT-013 current
+  double Irms = emon1.calcIrms(SCT013_NUM_SAMPLES);
+
+  // Debug print
+  Serial.print("Current (Irms): ");
   Serial.print(Irms, 3);
   Serial.println(" A");
 
-  if (Irms > 0.5) {
-    digitalWrite(D4, HIGH);
+  // Send to Flask server
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    String url = "http://" + String(flask_server_ip) + ":" + String(flask_port) + "/update";
+    http.begin(client, url);
+    http.addHeader("Content-Type", "application/json");
+
+    String payload = "{\"field5\":" + String(Irms, 3) + "}";
+
+    int httpCode = http.POST(payload);
+    Serial.print("POST Status Code: ");
+    Serial.println(httpCode);
+
+    if (httpCode > 0) {
+      String response = http.getString();
+      Serial.println("Server Response: " + response);
+    } else {
+      Serial.printf("HTTPClient Error: %s\n", http.errorToString(httpCode).c_str());
+    }
+    http.end();
   } else {
-    digitalWrite(D4, LOW);
+    Serial.println("WiFi not connected - data not sent.");
   }
 
-  delay(2000);
+  delay(5000);
 }
